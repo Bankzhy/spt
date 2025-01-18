5a249f7e1fd13c2657e866d23d4cd373f7e5c14f
zhang.rog <bankzhy@gmail.com>
2025-01-18 14:20:43 +0900

    spt

--- sources/downstream_tasks/clone.py
+++ sources/downstream_tasks/clone.py
@@ -54,7 +54,7 @@ def run_clone(
         if split == 'valid':
             datasets[split] = datasets[split].subset(0.1)
         if split == 'test':
-            datasets[split] = torch.utils.data.Subset(datasets[split], list(range(180001, 240000)))
+            datasets[split] = torch.utils.data.Subset(datasets[split], list(range(240001, 300000)))
         # datasets[split] = datasets[split].subset(0.1)
         logger.info(f'The size of {split} set: {len(datasets[split])}')
     if args.train_subset_ratio and 'train' in datasets:
